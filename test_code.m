1910f69a2a5c95379111d5d2dee7f50941acec4a
Karan Shah <kbshah1998@outlook.com>
2018-03-14 20:53:20 +0530

    Upgrade: Canny
    Replaced with canny
    Tweaked
    Add logger
    Add cirlabel

--- test_code.m
+++ test_code.m
@@ -1,35 +1,37 @@
+clc, clear;
+url = 'http://192.168.43.1:8080/shot.jpg';
+
 
 %% Clear environment vars
 % Image Pre-processing and plane trisection
-
-clc, clear;
-image = imread('<image>.png');
-h = [-1, -1, -1, -1,-1;-1, -1, -1, -1,-1;-1, -1, 23.8, -1,-1;-1, -1, -1, -1,-1;-1, -1, -1, -1,-1];
-image = imfilter(image, h, 'conv');
-fudgeFactor = 0.9;
+tic;
+original_img = imread(url);
+convolver_map = [-1, -1, -1, -1,-1;-1, -1, -1, -1,-1;-1, -1, 23.8, -1,-1;-1, -1, -1, -1,-1;-1, -1, -1, -1,-1];
+image = imfilter(original_img, convolver_map, 'conv');
+fudgeFactor = 4;
 se90 = strel('line', 2, 90);
 se0 = strel('line', 1, 0);
-%image = imsharpen(image, 'Amount', 2);
+
 rmat = image(:,:,1);
 gmat = image(:,:,2);
 bmat = image(:,:,3);
-%% Apply Sobel+Image Dilation+Filters on all RGB planes
+%% Apply canny+Image Dilation+Filters on all RGB planes
 
-%%rmat sobel
-[~, threshold] = edge(rmat, 'sobel');
-BWs = edge(rmat,'sobel', threshold * fudgeFactor);
+%%rmat canny
+[~, threshold] = edge(rmat, 'canny');
+BWs = edge(rmat,'canny', threshold * fudgeFactor);
 BWsdilr = imdilate(BWs, [se90 se0]);
 %figure, imshowpair(BWsdilr, BWs, 'montage'), title('dilated R gradient mask');
 
-%%gmat sobel
-[~, threshold] = edge(gmat, 'sobel');
-BWs = edge(gmat,'sobel', threshold * fudgeFactor);
+%%gmat canny
+[~, threshold] = edge(gmat, 'canny');
+BWs = edge(gmat,'canny', threshold * fudgeFactor);
 BWsdilg = imdilate(BWs, [se90 se0]);
 %figure, imshowpair(BWsdilg, BWs, 'montage'), title('dilated G gradient mask');
 
-%%bmat sobel
-[~, threshold] = edge(bmat, 'sobel');
-BWs = edge(bmat,'sobel', threshold * fudgeFactor);
+%%bmat canny
+[~, threshold] = edge(bmat, 'canny');
+BWs = edge(bmat,'canny', threshold * fudgeFactor);
 BWsdilb = imdilate(BWs, [se90 se0]);
 %figure, imshowpair(BWsdilb, BWs, 'montage'), title('dilated B gradient mask');
 
@@ -37,9 +39,30 @@ BWsdilb = imdilate(BWs, [se90 se0]);
 
 c=BWsdilr+BWsdilg+BWsdilb;
 c = ~c;
-detectCircles = @(x) imfindcircles(x,[10 25],'Sensitivity',0.84, 'EdgeThreshold',0.1, 'Method','PhaseCode', 'ObjectPolarity','Bright');
+
+detectCircles = @(x) imfindcircles(x,[10 25],'Sensitivity',0.84, 'EdgeThreshold',0.0, 'Method','PhaseCode', 'ObjectPolarity','Bright');
 [centers, radii, metric] = detectCircles(c);
 
 %% Mark circles on the image
-imshow(c);
-viscircles(centers, radii);
+imshow(original_img);
+viscircles(centers, radii,'LineStyle','-.', 'LineWidth',1.0);
+
+for k = 1:length(metric)
+    
+    metricB1_string = sprintf('%d',k);
+    text(centers(k,1),centers(k,2),metricB1_string,'color','y','HorizontalAlignment', 'center','VerticalAlignment', 'middle');
+end
+number=length(centers);
+timeElapsed=toc; 
+%Execution Time Calculation
+%FileCreation
+fileID = fopen('log.txt','wt')
+fprintf(fileID,'Total Straws: %d \n',number);
+fprintf(fileID,'\nExecution Time in Program: %d seconds',timeElapsed);
+fprintf(fileID,'\nIndex \t Center pixel value \t Radii(pixels)');
+fprintf(fileID,'\n---------------------------------------------');
+for k = 1:length(centers)
+    fprintf(fileID,'\nStraw %d:\t%f\t%f',k,centers(k,1), radii(k,1));
+    
+end
+fclose(fileID);
