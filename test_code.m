8b23f9cd0dfd2cec648ed0f3a4aabcb59288a934
VivekTalwar <vivektalwar13071999@gmail.com>
2018-03-14 18:34:21 +0530

    Update test_code.m

--- test_code.m
+++ test_code.m
@@ -2,43 +2,60 @@
 %% Clear environment vars
 % Image Pre-processing and plane trisection
 
-clc, clear;
+tic;
+clear all;
+close all; 
+
 image = imread('3.jpg');
-%image = imresize(image, [1000, 1000]);
+image = imsharpen(image, 'Amount',4)
+number=0;
 fudgeFactor = 0.9;
-se90 = strel('line', 6, 90);
-se0 = strel('line', 2, 0);
-%image = imsharpen(image, 'Amount', 2);
+se90 = strel('line', 2, 90);
+se0 = strel('line', 1, 0);
+%Plane Trisection
 rmat = image(:,:,1);
 gmat = image(:,:,2);
 bmat = image(:,:,3);
-%% Apply Sobel+Image Dilation+Filters on all RGB planes
+%% Apply sobel+Image Dilation+Filters on all RGB planes
 
 %%rmat sobel
 [~, threshold] = edge(rmat, 'sobel');
 BWs = edge(rmat,'sobel', threshold * fudgeFactor);
 BWsdilr = imdilate(BWs, [se90 se0]);
+BWsdilr = medfilt2(BWsdilr);
+
 %figure, imshowpair(BWsdilr, BWs, 'montage'), title('dilated R gradient mask');
 
 %%gmat sobel
 [~, threshold] = edge(gmat, 'sobel');
 BWs = edge(gmat,'sobel', threshold * fudgeFactor);
 BWsdilg = imdilate(BWs, [se90 se0]);
+BWsdilg = medfilt2(BWsdilg);
+
 %figure, imshowpair(BWsdilg, BWs, 'montage'), title('dilated G gradient mask');
 
 %%bmat sobel
 [~, threshold] = edge(bmat, 'sobel');
 BWs = edge(bmat,'sobel', threshold * fudgeFactor);
 BWsdilb = imdilate(BWs, [se90 se0]);
+BWsdilb = medfilt2(BWsdilb);
+
 %figure, imshowpair(BWsdilb, BWs, 'montage'), title('dilated B gradient mask');
 
-%% Merge all dilated planes and count circles
+%%view all
+%% Merge all dilated planes and count circles 
 
 c=BWsdilr+BWsdilg+BWsdilb;
-c = ~c;
-detectCircles = @(x) imfindcircles(x,[30 100],'Sensitivity',0.85, 'EdgeThreshold',0.1, 'Method','PhaseCode', 'ObjectPolarity','Bright');
-[centers, radii, metric] = detectCircles(c);
+c = im2bw(c);
+c=~c;
+[centers, radii] = imfindcircles(c,[40 80], 'ObjectPolarity','dark', ...
+          'Sensitivity',0.92,'Method','twostage');
+h = viscircles(centers,radii);
+disp(size(centers));
+number=number+length(centers);
+timeElapsed=toc;
+fileID = fopen('strawcounter.txt','w')
+fprintf(fileID,'The number of straws counted: %d \n',number);
+fprintf(fileID,'\nExecution Time in Program: %d \n',timeElapsed);
+fclose(fileID);
 
-%% Mark circles on the image
-imshow(c);
-viscircles(centers, radii);
